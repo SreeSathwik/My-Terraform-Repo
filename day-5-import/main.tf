a47c4e492bc34ca9e862af5cb423c7e28d5d259e
SreeSathwik <adlurisathwik11@gmail.com>
2024-03-20 16:17:15 +0530

    import updates

--- day-5-import/main.tf
+++ day-5-import/main.tf
@@ -1,11 +1,12 @@
 resource "aws_instance" "stark" {
  ami           = "ami-013168dc3850ef002"#(we need to add from state file reference)
  instance_type = "t2.micro" #(we need to add from state file reference)
+ subnet_id = "subnet-0d22a30434299b20b"
  #rest all attributes need to be added to match actual resource presence in AWS 
 }
 
 
 # Command 
-terraform import aws_instance.stark  <instance id 
+#terraform import aws_instance.stark  <instance id 
 
-Note : Here instance id is manually crrated onw 
+#Note : Here instance id is manually crrated onw 
